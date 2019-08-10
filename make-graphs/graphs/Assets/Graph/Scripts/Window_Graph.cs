6b39fa5ddc4273d133333ebf121b07e9d4852c7a
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2019-08-10 20:21:08 +0530

    generating dots in a graph

---
+++ make-graphs/graphs/Assets/Graph/Scripts/Window_Graph.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Window_Graph : MonoBehaviour
+{
+    private RectTransform graphContainer;
+    [SerializeField]
+    private Sprite circleSprite;
+
+    private void Awake()
+    {
+        graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
+        //CreateCircle(new Vector2(200,200));
+        List<int> valueList = new List<int>() { 5, 98, 56, 45, 30, 22, 17, 15, 13, 17, 25, 37, 40, 36, 33 };
+        ShowGraph(valueList);
+    }
+
+    private void CreateCircle(Vector2 anchoredPosition)
+    {
+        GameObject gameObject = new GameObject("circle", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().sprite = circleSprite;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = anchoredPosition;
+        rectTransform.sizeDelta = new Vector2(11, 11);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+
+    }
+
+    private void ShowGraph(List<int> valueList)
+    {
+        float graphHeight = graphContainer.sizeDelta.y;
+        float xSize = 50f;
+        float yMaximum = 100f;
+        for (int i= 0; i < valueList.Count;++i)
+        {
+            float xposition = xSize+i * xSize;
+            float yposition = (valueList[i] / yMaximum) * graphHeight;
+            CreateCircle(new Vector2(xposition, yposition));
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
